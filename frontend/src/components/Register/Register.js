9b3bc7f6b3d2b63545a6f6320307f65c9eab6f10
jesminmofi-cse <162531919+jesminmofi-cse@users.noreply.github.com>
2025-05-21 16:56:44 +0530

    Register.js

--- frontend/src/components/Register/Register.js
+++ frontend/src/components/Register/Register.js
@@ -42,7 +42,7 @@ function Register({ darkMode ,setIsLoggedIn}) {
       return;
     }
     try{
-      const res = await fetch('http://localhost:5000/api/auth/register', {
+      const res = await fetch('https://color-gradient-app.onrender.com/api/auth/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
