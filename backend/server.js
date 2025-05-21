2fc6377e84ee660e137ceb39ec8d74429f848cb9
jesminmofi-cse <162531919+jesminmofi-cse@users.noreply.github.com>
2025-05-21 17:51:39 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -18,15 +18,17 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(session({
-  secret: 'yourSecretKey',
+  secret: process.env.SESSION_SECRET || 'yourSecretKey',
   resave: false,
   saveUninitialized: false,
   cookie: {
-   secure:true,
-    sameSite:'none',
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'none',
+    httpOnly: true,
   },
   store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
 }));
+
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
