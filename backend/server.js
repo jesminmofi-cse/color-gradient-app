1561f7f66cbe85cacfeab0004c5f23372b1b34a3
jesminmofi-cse <162531919+jesminmofi-cse@users.noreply.github.com>
2025-05-21 17:20:06 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,10 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-   secure:false
+   secure:true,
+    sameSite:'none',
   },
-  store: MongoStore.create({ mongoUrl: process.env.MONGO_URI })
+  store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
 }));
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
