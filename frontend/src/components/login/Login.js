4ceff6cb2b8489cbe4bf87268a03e6fd84b9b0a1
jesminmofi-cse <162531919+jesminmofi-cse@users.noreply.github.com>
2025-05-21 16:56:12 +0530

    Login.js

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -16,7 +16,7 @@ const Login = ({ darkMode, setIsLoggedIn }) => {
     setError('');
     console.log("Sending login request with:", { email, password });
     try {
-      const res = await fetch('http://localhost:5000/api/auth/login', {
+      const res = await fetch('https://color-gradient-app.onrender.com/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
