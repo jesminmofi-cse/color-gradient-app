83ea6523f197131b3642feee9cf0130abfdacdd6
jesminmofi-cse <162531919+jesminmofi-cse@users.noreply.github.com>
2025-05-21 16:59:13 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 const DB_URL = process.env.MONGO_URI;
 
 app.use(cors({
-  origin:"http://localhost:3000",
+  origin:"https://color-gradient-app.vercel.app/",
   credentials:true, 
 }));
 app.use(express.json()); 
